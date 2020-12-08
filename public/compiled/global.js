ee9d3e1582790e2d44d8ed2a56dd0e7f01117d26
Arttu Manninen <arttu@kaktus.cc>
2020-12-08 10:26:35 +0200

    Linted with StandardJS auto fix

--- public/compiled/global.js
+++ public/compiled/global.js
@@ -1,12 +1,12 @@
 var GLOBAL = (function () {
-    function GLOBAL() {
-    }
-    GLOBAL.playerMode = function () {
-        return GLOBAL.MODE == 'PLAYER';
-    };
-    GLOBAL.editorMode = function () {
-        return GLOBAL.MODE == 'EDITOR';
-    };
-    GLOBAL.MODE = "PLAYER";
-    return GLOBAL;
-})();
+  function GLOBAL () {
+  }
+  GLOBAL.playerMode = function () {
+    return GLOBAL.MODE == 'PLAYER'
+  }
+  GLOBAL.editorMode = function () {
+    return GLOBAL.MODE == 'EDITOR'
+  }
+  GLOBAL.MODE = 'PLAYER'
+  return GLOBAL
+})()
