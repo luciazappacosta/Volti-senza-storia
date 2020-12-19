d5e87e5cf3cf632969a330d2391440f05b9cbea8
Arttu Manninen <arttu@kaktus.cc>
2020-12-19 10:35:09 +0200

    Deprecated obsolete stuff

--- uploader/upload.sh
+++
@@ -1,8 +0,0 @@
-youtube-upload \
-	--client-secrets=client_secret.json \
-	--privacy=unlisted \
-	--description="http://www.exhaustingacrowd.com/" \
-	--title="Exhausting a Crowd" \
-	--title-template="{title} ({n}/{total})" \
-	--location="latitude=40.687730, longitude=-73.979779, altitude=0" \
-	/Volumes/LaCie/$1/*.MP4
